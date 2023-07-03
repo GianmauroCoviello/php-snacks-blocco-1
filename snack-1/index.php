8aa190ee2202d4f877893b026e9c2658be8a0ee2
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-03 20:42:16 +0200

    [DOCS] Strutturazione boilerplate esercizi. Inizio snack-1

---
+++ snack-1/index.php
@@ -0,0 +1,36 @@
+<?php
+
+    $arraySquadHome = [
+        [
+            'squad' => 'Olimpia milano',
+            'score' => '89'
+            
+        ],
+        [
+            'squad' => 'Virtus Bologna',
+            'score' => '76'
+            
+        ],
+        [
+            'squad' => 'Tortona',
+            'score' => '55'
+            
+        ],
+        [
+            'squad' => 'Sassari',
+            'score' => '86'
+            
+        ],
+        [
+            'squad' => 'Venezia',
+            'score' => '58'
+            
+        ]
+    ];
+
+    var_dump($arraySquadHome)
+
+
+?>
+
+
