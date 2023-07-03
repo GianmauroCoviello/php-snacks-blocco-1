7df1f301b9f384d420cf6a525638ff520369760d
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-03 21:51:21 +0200

    [DOCS] Snack-1 completato

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,36 +1,66 @@
 <?php
+    // creazione array associativi con la specifica sintassi chiave => valore
+    $arrayGameBasket = [
+        [
+            'squad' => 'Olimpia milano - Virtus Bologna',
+            'score' => '89 - 76'
+            
+        ],
+        [
+            'squad' => 'Tortona - Basket Napoli',
+            'score' => '89 - 76'
 
-    $arraySquadHome = [
-        [
-            'squad' => 'Olimpia milano',
-            'score' => '89'
+            
             
         ],
         [
-            'squad' => 'Virtus Bologna',
-            'score' => '76'
+            'squad' => 'Venezia - Sassari',
+            'score' => '89 - 76'
+            
             
         ],
         [
-            'squad' => 'Tortona',
-            'score' => '55'
+            'squad' => 'Monza - Varese',
+            'score' => '89 - 76'
+            
             
         ],
         [
-            'squad' => 'Sassari',
-            'score' => '86'
+            'squad' => 'Brindisi - Sassuolo',
+            'score' => '89 - 76'
             
-        ],
-        [
-            'squad' => 'Venezia',
-            'score' => '58'
             
         ]
     ];
-
-    var_dump($arraySquadHome)
-
-
+    
 ?>
+
+<!-- inserimento dell'html nel file php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>GIRONE QUALIFICAZIONI EUROLEGA</h1>
+    <!-- inserialo la lista che coterrà le partite -->
+    <ul>
+        <!-- cicliamo l'array associativo con il ciclo foreach -->
+        <?php foreach($arrayGameBasket as $game) {?>
+            <!-- inseriamo i list item che verra ciclato  -->
+            <li>
+                <!-- stampiamo le chiavi dell'array  -->
+                <?php echo $game['squad']." | ".$game['score'] ?>
+                
+            </li>
+
+            
+                        
+        <?php } ?>
+    </ul>
+</body>
+</html>
 
 
