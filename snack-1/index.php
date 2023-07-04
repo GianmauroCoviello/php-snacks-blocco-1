16e5c77330addce272b017efd36e022d5c7bad37
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-04 11:58:39 +0200

    [DOCS] aggiunta cartella bonus snack-4

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,66 +1,65 @@
 <?php
-    // creazione array associativi con la specifica sintassi chiave => valore
-    $arrayGameBasket = [
-        [
-            'squad' => 'Olimpia milano - Virtus Bologna',
-            'score' => '89 - 76'
-            
-        ],
-        [
-            'squad' => 'Tortona - Basket Napoli',
-            'score' => '89 - 76'
+// creazione array associativi con la specifica sintassi chiave => valore
+$arrayGameBasket = [
+    [
+        'squad' => 'Olimpia milano - Virtus Bologna',
+        'score' => '89 - 76'
 
-            
-            
-        ],
-        [
-            'squad' => 'Venezia - Sassari',
-            'score' => '89 - 76'
-            
-            
-        ],
-        [
-            'squad' => 'Monza - Varese',
-            'score' => '89 - 76'
-            
-            
-        ],
-        [
-            'squad' => 'Brindisi - Sassuolo',
-            'score' => '89 - 76'
-            
-            
-        ]
-    ];
-    
+    ],
+    [
+        'squad' => 'Tortona - Basket Napoli',
+        'score' => '89 - 76'
+
+
+
+    ],
+    [
+        'squad' => 'Venezia - Sassari',
+        'score' => '89 - 76'
+
+
+    ],
+    [
+        'squad' => 'Monza - Varese',
+        'score' => '89 - 76'
+
+
+    ],
+    [
+        'squad' => 'Brindisi - Sassuolo',
+        'score' => '89 - 76'
+
+
+    ]
+];
+
 ?>
 
 <!-- inserimento dell'html nel file php -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>GIRONE QUALIFICAZIONI EUROLEGA</h1>
     <!-- inserialo la lista che coterrà le partite -->
     <ul>
         <!-- cicliamo l'array associativo con il ciclo foreach -->
-        <?php foreach($arrayGameBasket as $game) {?>
+        <?php foreach ($arrayGameBasket as $game) { ?>
             <!-- inseriamo i list item che verra ciclato  -->
             <li>
                 <!-- stampiamo le chiavi dell'array  -->
-                <?php echo $game['squad']." | ".$game['score'] ?>
-                
+                <?php echo $game['squad'] . " | " . $game['score'] ?>
+
             </li>
 
-            
-                        
         <?php } ?>
     </ul>
 </body>
+
 </html>
-
-
--- snack-2/index.php
+++ snack-2/index.php
@@ -1,47 +1,40 @@
 <?php
+// dichiariamo una variabile di controllo
+$results = false;
 
-    // inseriamo tutte le variabili in GET facendo riferimento agli input della form in isset che avrà il compito di controllare se le variabili sono presenti o se hanno un valore Null
-    if(isset($_GET['name']) && isset($_GET['email']) && isset($_GET['age'])){
-        // dichiarazione variabili
-        $name = $_GET['name'];
-        $email = $_GET['email'];
-        $age = $_GET['age'];
-        $results = true;
+// inseriamo tutte le variabili in GET facendo riferimento agli input della form in isset che avrà il compito di controllare se le variabili sono presenti o se hanno un valore Null
 
-        
-        
+if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['age'])) {
+    // dichiarazione variabili
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $age = $_GET['age'];
 
-    
-        // inseriamo le diverse condizioni che la form dovra essere sottoposta
-        if ((strlen($name) > 3) && strpos($email, '@') && strpos($email, '.') && is_numeric($age) ) {
-            // se le condizioni sono tutte verificate allora results avrà il seguente valore 
-            $results = 'Accesso consentito';
-            
-        }
-        else{
-            // altrimenti
-            $results= ' Accesso negato';
-            
 
-        }
 
-        
-        
-    
-    
+    // inseriamo le diverse condizioni che la form dovra essere sottoposta
+    if ((strlen($name) > 3) && strpos($email, '@') && strpos($email, '.') && is_numeric($age)) {
+        // se le condizioni sono tutte verificate allora results avrà il seguente valore 
+        $results = 'Accesso consentito';
+    } else {
+        // altrimenti
+        $results = ' Accesso negato';
     }
-   
+}
+
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <!-- inseriamo il form -->
     <form action="index.php" method="GET">
@@ -51,14 +44,11 @@
         <button>INVIA DATI</button>
     </form>
     <h3>
-        
-        <?php
-            // chiamata della variabile condizionale
-            echo $results;
-               
-        ?>
+        <!-- chiamata della variabile condizionale -->
+        <?php echo $results; ?>
     </h3>
-    
+
 
 </body>
+
 </html>
